a2eac78578d3e6a7bbecab0e27c1e8a9b4623725
Amber Joy <amberjoy@Ambers-MacBook-Pro.local>
2016-01-20 23:11:56 -0600

    I don't know what's wrong but i feel like i'm getting closer here

--- client/client.js
+++ client/client.js
@@ -2,37 +2,40 @@ var app = angular.module('myApp', []);
 
 console.log('client hit');
 
-app.controller('TwitController', ['$http','$scope', function($http, $scope){
+app.controller('twitController', ['$http', '$scope', function($http, $scope) {
 
-    console.log('controller hit');
+    nounArray = [];
+    adjectiveArray = [];
+    $scope.twits = [];
 
-    $http.get('/adjectives').then(function(response){
-        var theAdj = response.data.adjectives;
-        var random = randomNumber(0, theAdj.length);
 
-        $scope.randAdj = function (){
-            for (i=0, i>theAdj.length, i++){
-                return theAdj;
-            };
-        };
-        return random;
-        //    for (i = 0; i > nouns.length; i++);
-        //    return nouns[random];
-
+    $http.get('/adjectives').then(function (response) {
+        var pickAdj = response.data.adjectives;
+        var random = randomNumber(0, pickAdj.length);
+        for (i = 0; i > pickAdj.length; i++) {
+            adjectiveArray.push(pickAdj[random]);
+        }
     });
 
-    //$http.get('/nouns').then(function(results){
-    //    $scope.nouns = results.data.nouns;
-        //console.log(results);
-        //$scope.randomNoun = function(){
-        //    var random = randomNumber(0, nouns.length-1);
-        //    for (i = 0; i > nouns.length; i++);
-        //    return nouns[random];
-        //}
-    //});
+    $http.get('/nouns').then(function (response) {
+        var pickNoun = response.data.nouns;
+        var random = randomNumber(0, pickNoun.length);
+        for (i = 0; i > pickNoun.length; i++) {
+            nounArray.push(pickNoun[random]);
+        }
+    });
+
+    $scope.twit = function(){
+        for (i = 0; i > 20; i++) {
+            twitArray.push(adjectiveArray[i] + nounArray[i]);
+        }
+        return twitArray;
+    };
+
 
 }]);
 
 
+//Random Number Generator
 function randomNumber (min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);}
