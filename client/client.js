d0b6b857f70c92aca59748d71e6177bd4db7595f
Amber Joy <amberjoy@Ambers-MacBook-Pro.local>
2016-01-22 14:27:15 -0600

    not sure what's going on here

--- client/client.js
+++ client/client.js
@@ -8,43 +8,43 @@ app.controller('twitController', ['$http', '$scope', function($http, $scope) {
     var adjectiveArray = [];
     var twitArray = [];
 
-    function getAdjectives() {
-    $http.get('/adjectives').then(function (response) {
-        var pickAdj = response.data.adjectives;
-        var random = randomNumber(0, pickAdj.length);
+    function getWords() {
+        $http.get('/adjectives').then(function (response) {
+            var pickAdj = response.data.adjectives;
+            var random = randomNumber(0, pickAdj.length);
 
-        for (i = 0; i < pickAdj.length; i++) {
-            adjectiveArray.push(pickAdj[random].adj);
-        }
-        console.log('Adj array:', adjectiveArray);
-    })};
+            for (i = 0; i < pickAdj.length; i++) {
+                adjectiveArray.push(pickAdj[random].adj);
+            }
+            console.log('Adj array:', adjectiveArray);
+        });
 
-    function getNouns() {
-    $http.get('/nouns').then(function (response) {
-        var pickNoun = response.data.nouns;
-        var random = randomNumber(0, pickNoun.length);
+        $http.get('/nouns').then(function (response) {
+            var pickNoun = response.data.nouns;
+            var random = randomNumber(0, pickNoun.length);
 
-        for (i = 0; i < pickNoun.length; i++) {
-            nounArray.push(pickNoun[random].noun);
-        }
-        console.log('Noun array:', nounArray);
-    })};
+            for (i = 0; i < pickNoun.length; i++) {
+                nounArray.push(pickNoun[random].noun);
+            }
+            console.log('Noun array:', nounArray);
+        });
+    };
 
     $scope.getTwits = function(){
         console.log('click');
-        getAdjectives();
-        getNouns();
+        getWords();
         for (i = 0; i < 10; i++) {
             twitArray.push(adjectiveArray[i] + nounArray[i]);
         }
         $scope.twits = twitArray;
-        return $scope.twits;
-
+        console.log('twitArray:', twitArray);
     };
 
 }]);
 
-
 //Random Number Generator
 function randomNumber (min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);}
+
+
+
