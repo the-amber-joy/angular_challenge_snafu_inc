b5a46d37211054052a667e9179abf31b89d261e3
Amber Joy <amberjoy@Ambers-MacBook-Pro.local>
2016-01-22 14:14:50 -0600

    fill nounarray and adjarray but with duplicates. append to dom but NaN.

--- client/client.js
+++ client/client.js
@@ -12,28 +12,29 @@ app.controller('twitController', ['$http', '$scope', function($http, $scope) {
     $http.get('/adjectives').then(function (response) {
         var pickAdj = response.data.adjectives;
         var random = randomNumber(0, pickAdj.length);
+
         for (i = 0; i < pickAdj.length; i++) {
             adjectiveArray.push(pickAdj[random].adj);
         }
-        console.log('pick adj:', pickAdj[random].adj);
         console.log('Adj array:', adjectiveArray);
     })};
 
     function getNouns() {
     $http.get('/nouns').then(function (response) {
-        var tempNouns = [];
         var pickNoun = response.data.nouns;
         var random = randomNumber(0, pickNoun.length);
-        for (i = 0; i > pickNoun.length; i++) {
-            tempNouns.push(pickNoun[random]);
+
+        for (i = 0; i < pickNoun.length; i++) {
+            nounArray.push(pickNoun[random].noun);
         }
+        console.log('Noun array:', nounArray);
     })};
 
     $scope.getTwits = function(){
         console.log('click');
         getAdjectives();
         getNouns();
-        for (i = 0; i > 10; i++) {
+        for (i = 0; i < 10; i++) {
             twitArray.push(adjectiveArray[i] + nounArray[i]);
         }
         $scope.twits = twitArray;
