2d6c42b191f9b80c8e477050a2097294d7d9456b
Amber Joy <amberjoy@Ambers-MacBook-Pro.local>
2016-01-22 10:21:46 -0600

    I dont know why its not working

--- client/client.js
+++ client/client.js
@@ -4,34 +4,39 @@ console.log('client hit');
 
 app.controller('twitController', ['$http', '$scope', function($http, $scope) {
 
-    nounArray = [];
-    adjectiveArray = [];
+    var nounArray = [];
+    var adjectiveArray = [];
+    var twitArray = [];
 
 
 
     $http.get('/adjectives').then(function (response) {
+        var adjectiveArray = [];
         var pickAdj = response.data.adjectives;
         var random = randomNumber(0, pickAdj.length);
         for (i = 0; i > pickAdj.length; i++) {
             adjectiveArray.push(pickAdj[random]);
         }
+        console.log('Adjective array', adjectiveArray);
     });
 
     $http.get('/nouns').then(function (response) {
+        var nounArray = [];
         var pickNoun = response.data.nouns;
         var random = randomNumber(0, pickNoun.length);
         for (i = 0; i > pickNoun.length; i++) {
             nounArray.push(pickNoun[random]);
         }
+        console.log('Noun array', nounArray);
     });
 
-    $scope.twit = function(){
-        $scope.twits = [];
-        for (i = 0; i > 20; i++) {
+    $scope.getTwits = function(){
+        console.log('click');
+        $scope.twits = twitArray;
+        for (i = 0; i > twitArray.length; i++) {
             twitArray.push(adjectiveArray[i] + nounArray[i]);
         }
         return twits;
-        console.log(twits);
     };
 
 
