386e3e794cc0435d68438e68adf1313c32e9784b
Amber Joy <amberjoy@Ambers-MacBook-Pro.local>
2016-01-22 22:29:43 -0600

    adjective array populates with 10 different random words. Noun Array does not.

--- client/client.js
+++ client/client.js
@@ -6,9 +6,9 @@ var twitArray = [];
 
 app.controller('twitController', ['$http', '$scope', function($http, $scope) {
 
-    $scope.getTwits = function(){
+    $scope.getTwits = function () {
         for (i = 0; i < 10; i++) {
-            twitArray.push(adjectiveArray[i]nounArray[i]);
+            twitArray.push(adjectiveArray[i] + nounArray[i]);
         }
         $scope.twits = twitArray;
         console.log('twitArray:', twitArray);
@@ -17,29 +17,33 @@ app.controller('twitController', ['$http', '$scope', function($http, $scope) {
     getWords();
 
     function getWords() {
+
         $http.get('/adjectives').then(function (response) {
             var pickAdj = response.data.adjectives;
-            var randomAdj = randomNumber(0, 10);
 
             for (i = 0; i < pickAdj.length; i++) {
+                var randomAdj = randomNumber(0, 10);
                 adjectiveArray.push(pickAdj[randomAdj].adj);
             }
             console.log('Adj array:', adjectiveArray);
         });
+
 
         $http.get('/nouns').then(function (response) {
             var pickNoun = response.data.nouns;
             var randomNoun = randomNumber(0, 20);
 
             for (i = 0; i < pickNoun.length; i++) {
+
                 nounArray.push(pickNoun[randomNoun].noun);
             }
             console.log('Noun array:', nounArray);
         });
-    };
 
 
-    function randomNumber (min, max){
-        return Math.floor(Math.random() * (1 + max - min) + min);}
+        function randomNumber(min, max) {
+            return Math.floor(Math.random() * (1 + max - min) + min);
+        }
 
+    }
 }]);
