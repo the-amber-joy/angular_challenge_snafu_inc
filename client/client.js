4c09405d9e815368de2d81efdda52597a7e969e7
Amber Joy <amberjoy@Ambers-MacBook-Pro.local>
2016-01-22 22:17:54 -0600

    App ALMOST works but the 10 handles generated are all exactly the same

--- client/client.js
+++ client/client.js
@@ -1,50 +1,45 @@
 var app = angular.module('myApp', []);
-
+var nounArray = [];
+var adjectiveArray = [];
+var twitArray = [];
 //console.log('client hit');
 
 app.controller('twitController', ['$http', '$scope', function($http, $scope) {
 
-    var nounArray = [];
-    var adjectiveArray = [];
-    var twitArray = [];
+    $scope.getTwits = function(){
+        for (i = 0; i < 10; i++) {
+            twitArray.push(adjectiveArray[i]nounArray[i]);
+        }
+        $scope.twits = twitArray;
+        console.log('twitArray:', twitArray);
+    };
+
+    getWords();
 
     function getWords() {
         $http.get('/adjectives').then(function (response) {
             var pickAdj = response.data.adjectives;
-            var random = randomNumber(0, pickAdj.length);
+            var randomAdj = randomNumber(0, 10);
 
             for (i = 0; i < pickAdj.length; i++) {
-                adjectiveArray.push(pickAdj[random].adj);
+                adjectiveArray.push(pickAdj[randomAdj].adj);
             }
             console.log('Adj array:', adjectiveArray);
         });
 
         $http.get('/nouns').then(function (response) {
             var pickNoun = response.data.nouns;
-            var random = randomNumber(0, pickNoun.length);
+            var randomNoun = randomNumber(0, 20);
 
             for (i = 0; i < pickNoun.length; i++) {
-                nounArray.push(pickNoun[random].noun);
+                nounArray.push(pickNoun[randomNoun].noun);
             }
             console.log('Noun array:', nounArray);
         });
     };
 
-    $scope.getTwits = function(){
-        console.log('click');
-        getWords();
-        for (i = 0; i < 10; i++) {
-            twitArray.push(adjectiveArray[i] + nounArray[i]);
-        }
-        $scope.twits = twitArray;
-        console.log('twitArray:', twitArray);
-    };
+
+    function randomNumber (min, max){
+        return Math.floor(Math.random() * (1 + max - min) + min);}
 
 }]);
-
-//Random Number Generator
-function randomNumber (min, max){
-    return Math.floor(Math.random() * (1 + max - min) + min);}
-
-
-
---
+++ client/test.js
@@ -0,0 +1,3 @@
+/**
+ * Created by amberjoy on 1/22/16.
+ */
